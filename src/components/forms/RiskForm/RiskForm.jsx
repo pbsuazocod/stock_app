9fb7837e06c58dd8555c9371b757e5df14a1ba16
pbsuazocod <pbsuazocod@gmail.com>
2022-10-17 20:03:49 -0800

    finish 2do form

--- src/components/forms/RiskForm/RiskForm.jsx
+++ src/components/forms/RiskForm/RiskForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useFormik } from "formik";
+import { Link } from "react-router-dom";
 
 // components
 import TextInput from "../../UI/FormInput/TextInput";
@@ -280,7 +281,7 @@ function RiskForm() {
       {/* -----------------------------formStep 3 */}
 
       {formStep === 3 && (
-        <div>
+        <div className="p-[2%] space-y-4">
           <h1 className="text-xl text-[#1A3B69] mb-[2%]">
             INFORMACIÓN FINANCIERA DEL CLIENTE
           </h1>
@@ -297,7 +298,7 @@ function RiskForm() {
               </div>
             </div>
 
-            <div className="mt-[2%] ml-[2%] border-l-4 pr-[2%] pl-[1%] border-[#40B879] first-line:border-solid">
+            <div className="mt-[2%] border-l-4 pr-[2%] pl-[1%] border-[#40B879] first-line:border-solid">
               <p>
                 SI SU RESPUESTA ES AFIRMATIVA ESPECIFIQUE EN QUÉ CONSISTEN
                 DICHAS INVERSIONES Y SUS MONTOS.
@@ -332,7 +333,7 @@ function RiskForm() {
               </div>
             </div>
 
-            <div className="mt-[2%] ml-[2%] border-l-4 pr-[2%] pl-[1%] border-[#40B879] first-line:border-solid">
+            <div className="space-y-3 mt-[2%]  border-l-4 pr-[2%] pl-[1%] border-[#40B879] first-line:border-solid">
               <p>
                 IDENTIFIQUE EL RANGO EN QUE SE ENCUENTRA LOS AÑOS DE EXPERIENCIA
                 EN LAS NEGOCIACIONES EN LOS MERCADOS DE VALORES:
@@ -408,8 +409,8 @@ function RiskForm() {
                   <label className="pl-2">NO</label>
                 </div>
               </div>
-              <div className="mt-[2%] lg:w-1/4 w-full border-l-4 pr-[2%] pl-[1%] border-[#40B879] first-line:border-solid">
-                <div>
+              <div className="grid grid-cols-3 mt-[2%] gap-4  w-full border-l-4 pr-[2%] pl-[1%] border-[#40B879] first-line:border-solid">
+                <div className="space-y-3">
                   PAÍS
                   <TextInput
                     formik={formik}
@@ -431,7 +432,7 @@ function RiskForm() {
                   />
                 </div>
 
-                <div>
+                <div className="space-y-3">
                   AÑOS DE EXPERIENCIA
                   <div className=" w-full">
                     <NumberInput
@@ -455,7 +456,7 @@ function RiskForm() {
                     />
                   </div>
                 </div>
-                <div>
+                <div className="space-y-3">
                   TIPO DE VALORES NEGOCIADOS
                   <TextInput
                     formik={formik}
@@ -478,10 +479,269 @@ function RiskForm() {
                 </div>
               </div>
             </div>
+
+            <div className="mb-[2%] mt-[2%] border-t-2 border-[#C1C1C1] " />
+            <div className="space-y-3 w-full border-l-4 pr-[2%] pl-[1%] border-[#9747FF] first-line:border-solid">
+              <p>
+                ¿CONOCE ALGUNAS DE LAS SIGUIENTES ALTERNATIVAS DE INVERSIÓN QUE
+                OFRECE EL MERCADO BURSÁTIL SALVADOREÑO? (HAGA UN COTEJO EN LAS
+                QUE CONOZCA)
+              </p>
+              <div className="flex items-center">
+                <input
+                  formik={formik}
+                  fieldName={"notes_and_eurobonds"}
+                  type="checkbox"
+                />
+                <label className="pl-2">Notas y Eurobonos.</label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  formik={formik}
+                  fieldName={"local_stocks"}
+                  type="checkbox"
+                />
+                <label className="pl-2">Acciones locales</label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  formik={formik}
+                  fieldName={"international_stocks"}
+                  type="checkbox"
+                />
+                <label className="pl-2">Acciones internacionales</label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  formik={formik}
+                  fieldName={"lots_and_state_bonds"}
+                  type="checkbox"
+                />
+                <label className="pl-2">Lotes y bonos del Estado</label>
+              </div>
+              <div className="flex items-center">
+                <input formik={formik} fieldName={"ishares"} type="checkbox" />
+                <label className="pl-2">Ishares</label>
+              </div>
+              <div className="flex items-center">
+                <input formik={formik} fieldName={"adrs"} type="checkbox" />
+                <label className="pl-2">ADRs</label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  formik={formik}
+                  fieldName={"investement_certificate"}
+                  type="checkbox"
+                />
+                <label className="pl-2">Certificados de Inversión</label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  formik={formik}
+                  fieldName={"stock_papers"}
+                  type="checkbox"
+                />
+                <label className="pl-2">Papeles Bursátiles</label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  formik={formik}
+                  fieldName={"other_values"}
+                  type="checkbox"
+                />
+                <label className="pl-2">Otros valores</label>
+              </div>
+              <div className="flex items-center">
+                <input formik={formik} fieldName={"none"} type="checkbox" />
+                <label className="pl-2">Ninguno (Sólo indique)</label>
+              </div>
+              <p>
+                MARQUE EL PLAZO O TIEMPO EN QUE ESTÁ INTERESADO EN INVERTIR:
+              </p>
+
+              <div className="lg:w-1/4 w-full ">
+                <TextInput
+                  formik={formik}
+                  fieldName={"level_of_income"}
+                  label={""}
+                  placeholder={"Menos del 1 año "}
+                  isAlphabetic
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {/* -----------------------------formStep 4 */}
+
+      {formStep === 4 && (
+        <div className="p-[2%] space-y-4">
+          <h1 className="text-xl text-[#1A3B69] mb-[2%]">
+            INFORMACIÓN DE PERFIL DE RIESGO DEL CLIENTE
+          </h1>
+          <h2 className="text-xl text-[#1A3B69] mb-[2%]">
+            DE ACUERDO A SU CONOCIMIENTO Y EXPERIENCIA, ¿CUÁL ES SU PREFERENCIA
+            RESPECTO AL RIESGO?
+          </h2>
+          <div>
+            <div className="flex items-center">
+              <input
+                formik={formik}
+                fieldName={"conservative"}
+                type="checkbox"
+              />
+              <label className="pl-2">
+                Conservador o adverso al riesgo (Rentista):
+              </label>
+            </div>
+            <p className="pl-[2%] text-justify">
+              Busca una inversión segura en la que el riesgo de pérdida de
+              capital aportado sea mínimo pero a la vez una mayor probabilidad
+              de preservar el capital. Se le recomienda una inversión en valores
+              con altas calificaciones o en fondos de inversión de dinero, en
+              valores de renta fija del Estado o con buenas calificaciones, con
+              riesgo muy reducido de pérdida de valor del capital invertido,
+              siempre que mantiene la inversión en el largo plazo.
+            </p>
+          </div>
+          <div>
+            <div className="flex items-center">
+              <input formik={formik} fieldName={"moderate"} type="checkbox" />
+              <label className="pl-2">Moderado:</label>
+            </div>
+            <p className="pl-[2%] text-justify">
+              Persigue mantener la estabilidad de su inversión, dispuesto a
+              aceptar modestos retornos a cambio de tener mayor seguridad de sus
+              inversiones, o rentabilidad ligeramente superior a los tipos de
+              interés del mercado bancario. Busca un equilibrio entre
+              rentabilidad y riesgo.
+            </p>
+          </div>
+          <div>
+            <div className="flex items-center">
+              <input formik={formik} fieldName={"aggressive"} type="checkbox" />
+              <label className="pl-2">Agresivo o especulador</label>
+            </div>
+            <p className="pl-[2%] text-justify">
+              Son inversores que tienen conocimiento del mercado donde están
+              invirtiendo, manejan herramientas de análisis fundamental y
+              técnico, asumen los riesgos de sus inversiones, buscan maximizar
+              la rentabilidad e invierten básicamente en valores de renta
+              variable y renta fija de alto riesgo sin importar la volatilidad
+              extrema de los mercados y las pérdidas potenciales
+            </p>
+          </div>
+
+          <p>
+            ¿CUÁL ES EL OBJETIVO O PROPÓSITO FUNDAMENTAL DE SUS INVERSIONES?
+            (HAGA UN COTEJO)
+          </p>
+
+          <div className="flex items-center">
+            <input
+              formik={formik}
+              fieldName={"invest_risk_free"}
+              type="checkbox"
+            />
+            <label className="pl-2">
+              Invertir en valores libres de riesgo y fácilmente convertibles en
+              efectivo
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              formik={formik}
+              fieldName={"moderate_risks"}
+              type="checkbox"
+            />
+            <label className="pl-2">
+              Invertir en valores líquidos y con riesgos moderados.
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              formik={formik}
+              fieldName={"acumulate_capital"}
+              type="checkbox"
+            />
+            <label className="pl-2">
+              Lograr un rendimiento que permita acumular capital para financiar
+              metas futuras.
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              formik={formik}
+              fieldName={"high_profitability"}
+              type="checkbox"
+            />
+            <label className="pl-2">
+              Obtener rentabilidad alta aunque esto signifique mayor riesgo.{" "}
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              formik={formik}
+              fieldName={"others_explain"}
+              type="checkbox"
+            />
+            <label className="pl-2">Otro (explique):___________________ </label>
+          </div>
+          <p className="text-justify">
+            IDENTIFIQUE CUÁL SERÍA LA REACCIÓN O DECISIÓN QUE USTED TOMARÍA, EN
+            EL SUPUESTO QUE ESCUCHARA NOTICIAS NEGATIVAS DE LA SITUACIÓN
+            ECONÓMICA LOCAL O INTERNACIONAL, SI OBSERVA QUE LOS ACTIVOS
+            FINANCIEROS ESTÁN TENIENDO UN DESEMPEÑO NEGATIVO, O SI APRECIA QUE
+            ESTÁ TENIENDO PÉRDIDAS DE CAPITAL EN SUS INVERSIONES:
+          </p>
+          <div className="flex items-center">
+            <input
+              formik={formik}
+              fieldName={"moderate_risks"}
+              type="checkbox"
+            />
+            <label className="pl-2">
+              Invertir en valores líquidos y con riesgos moderados.
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              formik={formik}
+              fieldName={"acumulate_capital"}
+              type="checkbox"
+            />
+            <label className="pl-2">
+              Lograr un rendimiento que permita acumular capital para financiar
+              metas futuras.
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              formik={formik}
+              fieldName={"high_profitability"}
+              type="checkbox"
+            />
+            <label className="pl-2">
+              Obtener rentabilidad alta aunque esto signifique mayor riesgo.{" "}
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              formik={formik}
+              fieldName={"others_explain"}
+              type="checkbox"
+            />
+            <label className="pl-2">Otro (explique):___________________ </label>
           </div>
         </div>
       )}
 
+      {/* -----------------------------formStep 5 */}
+      {formStep === 5 && (
+        <Link to="/" className="m-[2%]">
+          <h1>EL FORMULARIO FUE ENVIADO EXITOSAMENTE.</h1>
+        </Link>
+      )}
       {/* buttons  */}
       {formStep < 5 ? (
         <div
